f56eaf368b5c9e98fe97e3eb07531c013df685e0
Pedro Cazalá <cazalapedro@gmail.com>
2022-03-22 14:19:50 -0300

    clase 9 completa

---
+++ clase9/ejercicio3/main.js
@@ -0,0 +1,30 @@
+//vi hasta 1,40
+const express =require('express')
+const app =express()
+//cargamos handlebars
+const handlebars = require('express-handlebars')
+
+app.engine('hbs',handlebars.engine({
+    extname:'.hbs',
+    defaultLayout:'index.hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials'
+}))
+
+app.set('view engine','hbs')
+app.set('views','./views')
+
+app.use(express.static('public'))
+
+
+app.get('/',(req,res)=>{
+    const datos={
+        titulo:'Prueba de plantilla',
+        mensaje:'Este es un mensaje',
+        autor:'Pedro Cazalá',
+        version:'1.1'
+    }
+    res.render('main',datos)
+})
+
+app.listen(8080,()=>console.log('corriendo'))
