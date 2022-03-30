1860c7d87282bb07b27a07669f22b7dbd5aba79e
Pedro Cazalá <cazalapedro@gmail.com>
2022-03-29 23:02:51 -0300

    entregable clase 12- falta mejorar

--- clase12/entregable/public/main.js
+++ clase12/entregable/public/main.js
@@ -1,4 +1,5 @@
 const socket = io.connect();
+// const moment = require('moment')
 
 function addProduct(e) {
     const product = {
@@ -12,3 +13,74 @@ function addProduct(e) {
     socket.emit('newProduct', product);
     return false;
 }
+function addMessage(e) {
+    const message = {
+        username: document.getElementById('username').value,
+        texto: document.getElementById('texto').value,
+        date: moment().format('DD-MM-YYYY HH:mm:ss'),
+        laal:'lala'
+    };
+    console.log(message)
+    document.getElementById('username').value = ''
+    document.getElementById('texto').value = ''
+    
+    socket.emit('newMessage', message);
+    return false;
+}
+
+
+function render(products) {
+    if(products[0]){
+        const crearTabla =
+                    `
+                    <table class="table table-dark">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Titulo</th>
+                            <th scope="col">Precio</th>
+                            <th scope="col">Imagen</th>
+                            </tr>
+                        </thead>
+                        <tbody id="productsTable">
+                        </tbody>
+                    </table>
+                    `
+        document.getElementById("table").innerHTML = crearTabla;
+        const html = products
+            .map((product, index) => {
+                return `
+                    <tr>
+                        <th scope="row">${product.id}</th>
+                        <td>${product.title}</td>
+                        <td>$${product.price}</td>
+                        <td><img src="${product.img}" class="card-img-top" alt="${product.title}" style="max-width:100px;"></td>
+                    </tr>
+                `;
+            })
+            .join(" ");
+        document.getElementById("productsTable").innerHTML = html;
+    }
+
+}
+socket.on("products", function (products) {
+    render(products);
+});
+
+function render(messages) {
+    if(messages[0]){
+        
+        const html = messages
+            .map((message, index) => {
+                return `
+                    <div>${message.username}</div>
+                `;
+            })
+            .join(" ");
+        document.getElementById("messageContainer").innerHTML = html;
+    }
+
+}
+socket.on("messages", function (messages) {
+    render(messages);
+});
--- clase12/entregable/server.js
+++ clase12/entregable/server.js
@@ -22,10 +22,11 @@ app.get('/',(req,res)=>{
     res.render('index',{products})
 })
 
+
 app.use(express.static(__dirname + '/public'));
 
 const fileProducts = './productos.txt'
-let products;
+let products = [];
 const getProducts = ()=>{
     try{
         products = fs.readFileSync(fileProducts,'utf-8')
@@ -36,15 +37,35 @@ const getProducts = ()=>{
 }
 getProducts()
 
-const update = ()=>{
-    products = JSON.stringify(products)
-    fs.writeFileSync(fileProducts,products)
+const updateProducts = ()=>{
+    const adsProducts = JSON.stringify(products)
+    fs.writeFileSync(fileProducts,adsProducts)
 
 }
 
 
 
 console.log(products);
+
+const fileMessages = './messages.txt'
+let messages = [];
+const getMessages = ()=>{
+    try{
+        messages = fs.readFileSync(fileMessages,'utf-8')
+        messages = JSON.parse(messages)
+    }catch{
+        fs.writeFileSync(fileMessages,'[]')
+    }
+}
+getMessages()
+
+const updateMessages = ()=>{
+    const adsMessages = JSON.stringify(messages)
+    fs.writeFileSync(fileMessages,adsMessages)
+
+}
+
+console.log('messages '+ messages);
 
 
 io.on("connection", function (socket) {
@@ -53,7 +74,15 @@ io.on("connection", function (socket) {
         const id = products.length + 1
         products.push({id,...newProducto})
         console.log(products);
-        update()
+        updateProducts()
         io.sockets.emit("products", products);
     });
+    socket.on('newMessage',(newMessage)=>{
+        // console.log(newMessage);
+        const id = messages.length + 1
+        messages.push({id,...newMessage})
+        console.log(messages);
+        updateMessages()
+        io.sockets.emit("messages", messages);
+    })
 })
