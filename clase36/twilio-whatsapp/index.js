8f206e0b4f99667feb9fdb8c1fceb3678292277e
Pedro Cazala <cazalapedro@gmail.com>
2022-07-11 11:26:12 -0300

    clase36: twilio whatsapp configurado para enviar mensajes al admin

---
+++ clase36/twilio-whatsapp/index.js
@@ -0,0 +1,23 @@
+require('dotenv/config')
+const accountSid = 'AC37d79788c122b2c5e991017acfee0911'; 
+const authToken = '498e2bb4c5bb14502a7645d324635ae0'; 
+const client = require('twilio')(accountSid, authToken); 
+
+// client.messages 
+//     .create({ 
+//        body: 'Mensaje desde nodejs, proyecto twilio whatsapp, clase36', 
+//        mediaUrl: [      'https://res.cloudinary.com/hdsqazxtw/image/upload/v1559681445/logo_coderhouse_2_bmqbet.png' ],
+//        from: 'whatsapp:+14155238886',       
+//        to: `whatsapp:${process.env.PHONE}` 
+//      }) 
+//     .then(message => console.log(message.sid)) 
+//     .done();
+
+client.messages 
+    .create({ 
+       body: 'Prueba con numero de celular en *variable de entorno*', 
+       from: 'whatsapp:+14155238886',       
+       to: `whatsapp:${process.env.PHONE}` 
+     }) 
+    .then(message => console.log(message.sid)) 
+    .done();
