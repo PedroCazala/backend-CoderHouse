e93ed42ac517245faf32ea5fd7e325155e2a1e98
Pedro Cazala <cazalapedro@gmail.com>
2022-08-19 16:39:53 -0300

    deno product handlers complete

--- clase48/entregable/src/routes/products.router.ts
+++ clase48/entregable/src/routes/products.router.ts
@@ -5,6 +5,8 @@ export const productsRouter = new Router()
 .get("/api/productos", ProductsHandlers.getAll)
 .get("/api/productos/:productId", ProductsHandlers.getOneProductsById)
 .post("/api/productos", ProductsHandlers.pushProduct)
+.put("/api/productos/:productId", ProductsHandlers.updateProduct)
+.delete("/api/productos/:productId", ProductsHandlers.delateProduct)
 // (ctx:Context)=>{
 //     ctx.response.body ={hola:'mundo de productos'}
 // }
